04a280420f5a437676ec8c98a92a8551a9563535
Helltaker1234 <ukendo3333@gmail.com>
2026-04-01 19:19:17 +0900

    feat: 가제보 연결 성공
    move_group_sim.launch.py >> move_group 서버 역시 use_sim_time:=true 로 설정이 되어야 함.

--- src/areum2_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ src/areum2_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -5,3 +5,6 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("A1_URDF", package_name="areum2_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
+
+
+# world > base_link 고정해주는 역할.

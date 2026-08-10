05b6431a3e2564d084050b61026b4b8dd384f3b9
Helltaker1234 <ukendo3333@gmail.com>
2026-08-10 23:21:02 +0900

    feat: isaac sim 에 C1 과 Depth 를 올리기 위한 설정 완료. 또한 demo.launch 실행 시 hardware type 을 launch argument 로 사용자가 지정할 수 있게끔 함.

--- src/areum_ii_moveit_config/launch/demo.launch.py
+++ src/areum_ii_moveit_config/launch/demo.launch.py
@@ -1,7 +1,38 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_demo_launch
 
+from launch.actions import DeclareLaunchArgument
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+
+
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("areumii", package_name="areum_ii_moveit_config").to_moveit_configs()
-    return generate_demo_launch(moveit_config)
+
+    declare_hardware_type = DeclareLaunchArgument(
+        "ros2_control_hardware_type",
+        default_value="mock_components",
+        description="ROS2 control hardware interface type to use for the launch file -- possible values: [mock_components, real_hardware, isaac]",
+    )
+
+    hardware_type = LaunchConfiguration("ros2_control_hardware_type")
+
+    moveit_config =( 
+        MoveItConfigsBuilder(
+            "areumii", 
+            package_name="areum_ii_moveit_config"
+        )
+        .robot_description(
+            mappings={"ros2_control_hardware_type" : hardware_type,}
+        )
+        .to_moveit_configs()
+    )
+
+    demo_launch = generate_demo_launch(moveit_config)
+
+    return LaunchDescription(
+        [
+            declare_hardware_type,
+            *demo_launch.entities,
+        ]
+    )
